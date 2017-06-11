3b764b0ae5236b22d1b633d85a1549aaadb0e39d
Matthew Chick <chikmatthew@gmail.com>
2017-06-12 09:53:51 +1000

    Add some components by using ng g c recipes --spec false

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by MatthewChick on 12/6/2017.
+ * header is user-defined component manually
+ */
+
+import { Component } from '@angular/core';
+
+// Register HeaderComponent in app.module.ts, otherwise has an error
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+
+export class HeaderComponent {
+
+}
