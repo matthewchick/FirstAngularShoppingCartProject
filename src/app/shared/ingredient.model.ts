571a2474056cf54ef2386961ee824e92f8c88ed2
Matthew Chick <chikmatthew@gmail.com>
2017-06-12 21:32:19 +1000

    Add ingredient model and make a shortcut to represent instance variables using constructor

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,10 @@
+export class Ingredient {
+  // public name: string;
+  // public amount: number;
+
+  // it is a shortcut like declaring the above 2 statements
+  constructor(public name: string, public amount: number) {
+
+  }
+
+}
