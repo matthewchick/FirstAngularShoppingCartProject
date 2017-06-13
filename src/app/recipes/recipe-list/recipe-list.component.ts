c096ce491f187dd062fa9ce6f4f2c79b52e9f6d3
Matthew Chick <chikmatthew@gmail.com>
2017-06-13 22:25:12 +1000

    Use Event and Property Binding from recipe-item => recipe-list => recipe => recipe-list => recipe-detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  // recipe data will be exposed to recipes.component
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   // array of object => Recipe
   // static images can be stored in assets folder
   recipes: Recipe[] = [
@@ -22,6 +24,11 @@ export class RecipeListComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(selectedrecipeItem: Recipe) {
+    this.recipeWasSelected.emit(selectedrecipeItem);
+    console.log("recipeWasSelected's content is " + selectedrecipeItem.name);
   }
 
 }
