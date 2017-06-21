aa3561bd56e74838554c05d1d6922aab9ed03664
Matthew Chick <chikmatthew@gmail.com>
2017-06-21 19:40:24 +1000

    Add navigation by using routerLink

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,11 +11,13 @@ import {Component, EventEmitter, Output} from '@angular/core';
 })
 
 export class HeaderComponent {
+  /* method 1: event binding
+     method 2: we do not need the following codes <= use angular routing
   // pass data to parent component => app.component.html
   @Output() featureSelected = new EventEmitter<string>();
 
   onSelect(feature: string) {
     this.featureSelected.emit(feature);
     console.log("feature's content is " + feature);
-  }
+  } */
 }
